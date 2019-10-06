05d4df807f7474012e39090d2f56b76f8bc83e83
Alex Tutunnik <tutunikssa@gmail.com>
2019-10-06 21:11:53 +0300

    update two part pdo

---
+++ leasoft_train_plan/leasoft/work with pdo/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <?php session_start(); ?>
+    <?php include "#pdo#notate_list.php"?>
+    <title>Notate List</title></head>
+<body>
+<?php if (isset($_SESSION['username'])) {?>
+<div style="width: 100%; background: limegreen; text-align: center">
+    Hello broh <?php print $_SESSION['username']; ?>
+    <a href="?forgot_user=true"><input type="button" value="Забыть"></a>
+</div>
+<?php } ?>
+<table border="3px">
+    <tr><th>Id</th><th>Notate</th><th>Author</th><th></th></tr>
+    <?php  $local_notation = getNotationList(); foreach ($local_notation as $value => $item) { ?>
+        <tr>
+            <td><?php print $value;?></td>
+            <td><?php print $item['notate'];?></td>
+            <td><?php print $item['author'];?></td>
+            <td>
+                <form action="#" method="GET">
+                <input type="hidden" name="id" value=<?php print $value; ?>>
+                    <input type="submit" name="delete" value="Удалить">
+
+                </form>
+            </td>
+
+        </tr>
+    <?php } ?>
+
+</table>
+<form action="#pdo#database_functions.php" method="get">
+    Введите ваше имя : <input type="text" name="author_name"> Запомнить меня <input type="checkbox" name="remember"><br>
+Введите текст заметки : <input type="text" name="text_notate"><br>
+<input type="submit" name="submited" value="Добавить">
+</form>
+</body>
+</html>
